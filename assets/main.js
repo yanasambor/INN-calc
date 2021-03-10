f0a11c60a33336eccf3fa57cc5de6ae265f51d94
Artur <nk@itviz.biz>
2021-03-10 10:00:15 +0200

    second

--- assets/main.js
+++ assets/main.js
@@ -6,6 +6,20 @@ function calc(){
     
     let num = inn.slice(0,9);
 
+    let birthdayCheck = +inn.slice(0,5).join('');
+
+    let birthData = new Date (1899, 11, 31);
+
+    birthData.setDate(birthData.getDate()+ birthdayCheck);
+
+    let birthdayFormat = birthData.toLocaleDateString('ru-RU')
+
+    let today = Date.now();
+
+    let now = new Date(today);
+
+    let age = Math.floor((now - birthData)/31536000000.4);
+
     let summ = num.reduce(function(r,a,i){return r+a*coefficients[i]},0);
     
     let controlNumber = summ % 11;
@@ -30,8 +44,13 @@ function calc(){
         else {
             checkResult=("ИИН не является действительным");
             genderResult=("-");
+            birthdayFormat=("-");
+            age=("-");
         }
     
         check.innerHTML=checkResult;
         gender.innerHTML=genderResult;
+        birthday.innerHTML=birthdayFormat;
+        fullAge.innerHTML=age;
+
     }
